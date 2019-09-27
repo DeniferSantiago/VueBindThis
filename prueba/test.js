8cae472ccae16b58c1ffccfc04a066dc61098e81
Denifer <deniferjose05@gmail.com>
2019-09-27 15:30:46 -0400

    file to manually test the extension

---
+++ prueba/test.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:"#app",
+    data:{
+        bool: false,
+        num: 4,
+        obj: {
+            name: "Louis",
+            lastname:"walker"
+        },
+        arr: ["my", "name", "is", "skrillex"],
+        str: "comment"
+    },
+    methods:{
+        workingData(){
+            let double = this.num * this.num;
+        }
+    },
+    computed:{
+        action(){
+            return this.bool? "true": "false";
+        }
+    }
+})
