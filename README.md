5101102c5c8ee6588fd816d2b9b7338d8214fe2d
Denifer <deniferjose05@gmail.com>
2019-10-02 02:16:32 -0400

    cacheando el documento y actualizando la documentacion

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # VueBindThis
+
 bind this in a vue instance.
+
+## Usage:
+
+1. Install node with npm
+2. Run comand: ´npm install -g yo generator-code´
+3. Clone this proyect: ´git clone https://github.com/DeniferSantiago/VueBindThis.git´
+4. Open proyect with vs code
+5. Run command: ´npm install´ for get node_modules folder
+6. Explore and Contribute
+  
+## Debug:
+
+1. Press F5 for run extension into new instance of vs code
+2. In the debug instance open the project test folder
+3. You can work with the extension as a test within the prueba.js file
+
+## Donations:
+
+**Do you want to donate to reward project members?**  
+*If so, you can do it through paypal.*  
+[Donate with paypal](https://paypal.me/paycreatech?locale.x=es_XC)
--- extension.js
+++ extension.js
@@ -20,6 +20,41 @@ class PropCompletion{
 		let length = this.childs.push(new PropCompletion(compItem, type));
 		return this.childs[length];
 	}
+		/**
+	 * 
+	 * @param {Object} obj 
+	 */
+	static getPropCompletion(obj){
+		/**
+		 * @type{Array<PropCompletion>}
+		 */
+		var completionItems = [];
+		for (const key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				const element = obj[key];
+				let icon = vscode.CompletionItemKind.Property;
+				let item = new vscode.CompletionItem(key, icon);
+				let itemCompletion = new PropCompletion(item,typeof element);
+				if(typeof element === "object"){
+					if(!Array.isArray(element)){
+						itemCompletion.childs = PropCompletion.getPropCompletion(element);
+						itemCompletion.itemCompletion.commitCharacters = [".", ";", "="];
+					}
+				}
+				else if(typeof element === "number"){
+					itemCompletion.itemCompletion.commitCharacters = ["=", ";", "+", "-", "*", "/"];
+				}
+				else if(typeof element === "undefined" || typeof element === "boolean"){
+					itemCompletion.itemCompletion.commitCharacters = [";", "="];
+				}
+				else if(typeof element === "string"){
+					itemCompletion.itemCompletion.commitCharacters = ["=", ";", "+"];
+				}
+				completionItems.push(itemCompletion);
+			}
+		}
+		return completionItems;
+	}
 }
 class VueIntellisense{
 	/**
@@ -31,50 +66,84 @@ class VueIntellisense{
 		this.dataString = "";
 		this.methodsString = "";
 		this.computedString = "";
+		/**@type{vscode.Range} */
+		this.vueRange = null;
 		this.getVueString();
+		this.getDataString();
+		this.getMethodsString();
+		this.getComputedString();
+		this.dataObj = this.dataString === null? null : JSON.parse(`{${formatedText(this.dataString)}}`);
 	}
-	set lang(lang) {
-		this.language = lang;
+	getCompletionItems(){
+		let regex = new RegExp(/((?<!: *)(?<=( |\t)*)(\b[A-z]+)(?=(\(\)|:( |\t*)function *))(?!((\(\)\r?\n))|(\(\);)))/, "gs");
+		let methodsName = this.methodsString.match(regex);
+		let computedName = this.computedString.match(regex);
+		let data = this.dataObj.data || {};
+		/**
+		 * @type{Array<PropCompletion>}
+		 */
+		var completionItems = PropCompletion.getPropCompletion(data);
+		methodsName.forEach(name => {
+			let item = new vscode.CompletionItem(name, vscode.CompletionItemKind.Method);
+			let itemCompletion = new PropCompletion(item, "function");
+			completionItems.push(itemCompletion);
+		});
+		computedName.forEach(name => {
+			let item = new vscode.CompletionItem(name, vscode.CompletionItemKind.Variable);
+			let itemCompletion = new PropCompletion(item, "function");
+			completionItems.push(itemCompletion);
+		});
+		return completionItems;
 	}
 	getVueString(){
 		let doc = this.document;
-		/**@type{vscode.Position} */
-		var iniPos = null;
-		/**@type{vscode.Position} */
-		var finPos = null;
-		var llaves = -1;
-		var iniData = false;
-		for (let i = 0; i < doc.lineCount; i++) {
-			const line = doc.lineAt(i);
-			if(finPos !== null)
-				break;
-			else if(iniPos === null){
-				let posData = line.text.search(/((?<=\=( |	*))\bnew( ) *Vue)\b/);
-				iniPos = posData !== -1? new vscode.Position(i, posData) : null;
-			}
-			if(iniPos !== null){//no podemos usar else ya que posiblemente haya tomado el valor en la anterior pregunta.
-				line.text.split("").forEach((char, j) => {
-					if((j < iniPos.character  && i === iniPos.line) || finPos !== null)
-						return;
-					if(char === "{" || char === "("){
-						llaves++;
-						iniData = true;
-					}
-					else if(char === "}" || char === ")"){
-						llaves--;
-						if(iniData && llaves === -1)
-							finPos = new vscode.Position(i, j + 1);
-						else if(llaves < -1)
-							throw new Error("Error de sintaxis de la instancia 'Vue'. Está cerrando la llave sin haberla abierto.");
-					}
-				});
-			}
+		this.vueRange = getRangeText(doc.getText(), /((?<=\=( |	*))\bnew( ) *Vue)\b/);
+		if(this.vueRange === null)
+			this.vueString = null;
+		else
+			this.vueString = doc.getText(this.vueRange);
+	}
+	getDataString(){
+		let doc = this.document;
+		let dataRange = getRangeText(this.vueString, /\bdata *:/);
+		if(dataRange === null)
+			this.dataString = null;
+		else{
+			let posIni = new vscode.Position(this.vueRange.start.line + dataRange.start.line, dataRange.start.character);
+			let posFin = new vscode.Position(this.vueRange.start.line + dataRange.end.line, dataRange.end.character);
+			var dataText = doc.getText(new vscode.Range(posIni, posFin));
+			this.dataString = dataText;
 		}
-		var vueRange = new vscode.Range(iniPos, finPos);
-		var vueText = doc.getText(vueRange);
-		console.log(vueText);
+	}
+	getMethodsString(){
+		let doc = this.document;
+		let methodsRange = getRangeText(this.vueString, /\bmethods *:/);
+		if(methodsRange === null)
+			this.methodsString = null;
+		else{
+			let posIni = new vscode.Position(this.vueRange.start.line + methodsRange.start.line, methodsRange.start.character);
+			let posFin = new vscode.Position(this.vueRange.start.line + methodsRange.end.line, methodsRange.end.character);
+			var methodsText = doc.getText(new vscode.Range(posIni, posFin));
+			this.methodsString = methodsText;
+		}
+	}
+	getComputedString(){
+		let doc = this.document;
+		let computedRange = getRangeText(this.vueString, /\bcomputed *:/);
+		if(computedRange === null)
+			this.computedString = null;
+		else{
+			let posIni = new vscode.Position(this.vueRange.start.line + computedRange.start.line, computedRange.start.character);
+			let posFin = new vscode.Position(this.vueRange.start.line + computedRange.end.line, computedRange.end.character);
+			var computedText = doc.getText(new vscode.Range(posIni, posFin));
+			this.computedString = computedText;
+		}
 	}
 }
+/**
+ * @type{Array<PropCompletion>}
+ */
+var completions = [];
 /**
  * @type{VueIntellisense}
  */
@@ -107,58 +176,31 @@ function activate(context) {
 			provideCompletionItems(document, position) {
 				createVueIntellisense(document);
 				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('this.')) {
+				if(linePrefix[linePrefix.length -1] !== "."){
 					return undefined;
 				}
-				/**@type{vscode.Position} */
-				var iniPos = null;
-				/**@type{vscode.Position} */
-				var finPos = null;
-				var llaves = -1;
-				var iniData = false;
-				for (let i = 0; i < document.lineCount; i++) {
-					const line = document.lineAt(i);
-					if(finPos !== null)
-						break;
-					else if(iniPos === null){
-						let posData = line.text.search(/\bdata *:/);
-						iniPos = posData !== -1? new vscode.Position(i, posData) : null;
+				if (!linePrefix.endsWith('this.')) {
+					let objs = completions.filter(x => x.childs.length > 0);
+					for (let i = 0; i < objs.length; i++) {
+						const element = objs[i];
+						if(linePrefix.endsWith(element.itemCompletion.label + ".")){
+							let items = [];
+							element.childs.forEach(compItem => {
+								items.push(compItem.itemCompletion);
+							});
+							return items;
+						}
 					}
-					if(iniPos !== null){//no podemos usar else ya que posiblemente haya tomado el valor en la anterior pregunta.
-						line.text.split("").forEach((char, j) => {
-							if((j < iniPos.character && i === iniPos.line) || finPos !== null)
-								return;
-							if(char === "{"){
-								llaves++;
-								iniData = true;
-							}
-							else if(char === "}"){
-								llaves--;
-								if(iniData && llaves === -1)
-									finPos = new vscode.Position(i, j + 1);
-								else if(llaves < -1)
-									throw new Error("Error de sintaxis de la propiedad 'data'. Está cerrando la llave sin haberla abierto.");
-							}
-						});
-					}
+					return undefined;
 				}
-				
-				var dataRange = new vscode.Range(iniPos, finPos);
-				var dataText = document.getText(dataRange);
-				var textData = formatedText(dataText);
-				var objData = JSON.parse(`{${textData}}`);
-				var data = objData.data;
-				var completionItems = [];
-				for (const key in data) {
-					if (data.hasOwnProperty(key)) {
-						const element = data[key];
-						let icon = vscode.CompletionItemKind.Property;
-						let item = new vscode.CompletionItem(key, icon);
-						item.preselect = true;
-						completionItems.push(item);
-					}
-				}
-				return completionItems;
+				/**
+				 * @type {Array<vscode.CompletionItem>}
+				 */
+				var items = []; 
+				completions.forEach(compItem => {
+					items.push(compItem.itemCompletion);
+				});
+				return items;
 			}
 		},
 		'.'
@@ -205,13 +247,53 @@ function formatedText(unFormatedText) {
 	return textData;
 }
 /**
- * 
  * @param {vscode.TextDocument} document 
  */
 function createVueIntellisense(document, isUpdate = false) {
 	if(vueIntellisense === null || isUpdate){
 		vueIntellisense = new VueIntellisense(document);
+		completions = vueIntellisense.getCompletionItems();
 	}
+}
+/**
+ * 
+ * @param {String} doc 
+ * @param {RegExp} regex
+ */
+function getRangeText(doc, regex) {
+	/**@type{vscode.Position} */
+	var iniPos = null;
+	/**@type{vscode.Position} */
+	var finPos = null;
+	var llaves = -1;
+	var iniData = false;
+	var textLines = doc.split(/\r?\n/);
+	textLines.forEach((line, i) => {
+		if(finPos !== null)
+			return;
+		else if(iniPos === null){
+			let posData = line.search(regex);
+			iniPos = posData !== -1? new vscode.Position(i, posData) : null;
+		}
+		if(iniPos !== null){//no podemos usar else ya que posiblemente haya tomado el valor en la anterior pregunta.
+			line.split("").forEach((char, j) => {
+				if((j < iniPos.character  && i === iniPos.line) || finPos !== null)
+					return;
+				if(char === "{" || char === "("){
+					llaves++;
+					iniData = true;
+				}
+				else if(char === "}" || char === ")"){
+					llaves--;
+					if(iniData && llaves === -1)
+						finPos = new vscode.Position(i, j + 1);
+					else if(llaves < -1)
+						throw new Error("Error de sintaxis de la instancia 'Vue'. Está cerrando la llave sin haberla abierto.");
+				}
+			});
+		}
+	});
+	return new vscode.Range(iniPos, finPos);
 }
 exports.activate = activate;
 function deactivate() {}
